28b89751a492404faf2eabcf64e6b9ac61686de8
Seb Ringrose <sebringrose@gmail.com>
2021-05-14 14:27:28 +0100

    pageSize param & optimizations

--- getVideo/index.js
+++ getVideo/index.js
@@ -4,17 +4,18 @@ module.exports = async function (context, req) {
     const siteId = "c883736b-b8a6-4a02-8387-cf675a6a66ef"
     const apiKey = "e1d15419-0b6b-4672-b1bb-eb488ff996b3"
 
-    const response = await axios.get(`https://api.flowplayer.com/ovp/web/video/v2/site/${siteId}.json?api_key=${apiKey}&page_size=200&published=true`)
+    const name = req.query.name
+    const pageSize = req.query.pageSize
+    const response = await axios.get(`https://api.flowplayer.com/ovp/web/video/v2/site/${siteId}.json?api_key=${apiKey}&search=${name}&page_size=${pageSize ? pageSize : "200"}&published=true`)
     
     const videos = response.data.videos
-    const name = req.query.name
     const duration = req.query.duration
     const tags = req.query.tags ? req.query.tags.split(",") : false
     let filteredVideos
 
     if (name) {
-        // name provided - return specific video
-        filteredVideos = videos.filter(video => video.name === name)
+        // name provided - return search result
+        filteredVideos = videos
     } else {
         filteredVideos = duration || tags 
             // duration or tag query params present so filters are required
